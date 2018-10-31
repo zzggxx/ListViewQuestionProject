22a498f423af75d8071eb8a2182654960ccc7af5
Will <will.g.zhou@magugi.com>
2018-10-31 15:51:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/will/listviewquestionproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.will.listviewquestionproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        ImageAdapter adapter = new ImageAdapter(this, 0, Images.imageUrls);
+        listView.setAdapter(adapter);
+    }
+}
